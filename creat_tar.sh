9016c6704d0e4e07fc8c71ef3572e0387b9f6f65
Lakshmipathi <Lakshmipathi.G@giis.co.in>
2013-12-16 12:23:57 +0530

    creat_tar.sh finds version

--- creat_tar.sh
+++ creat_tar.sh
@@ -1,1 +1,2 @@
-git archive --prefix=giis-ext4/ master | bzip2 >giis-ext4_1.2.tar.bz2
+version=`grep argp_program_version src/giis-ext4.c | cut -f2 -d'=' | cut -f3 -d' '`
+git archive --prefix=giis-ext4/ master | bzip2 >giis-ext4_${version}.tar.bz2
